52565510e8a8aab9d8e99db1d7f2817a48d06975
Alex Uche <nnayidb@outlook.com>
2021-01-22 07:34:13 +0100

    new commit

---
+++ nnayifirstapp/src/main/java/com/nnayiapp/nnayifirstapp/NnayifirstappApplication.java
@@ -0,0 +1,13 @@
+package com.nnayiapp.nnayifirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnayifirstappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnayifirstappApplication.class, args);
+	}
+
+}
---
+++ nnayifirstapp/src/test/java/com/nnayiapp/nnayifirstapp/NnayifirstappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnayiapp.nnayifirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnayifirstappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
